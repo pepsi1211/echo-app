18d6c17725551904770b7e815f64c0ca5b3dd756
Jack <666@Jack.com>
2019-10-19 12:12:48 +0800

    微调

---
+++ echo/src/components/index/sublevel/Line.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="line">
+    
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return{
+
+    }
+  },
+  methods: {
+    
+    },
+  watch: {
+    
+    },
+  computed: {
+    
+    },
+  created() {
+    
+    },
+  mouted() {
+  
+  }
+}
+</script>
+<style scoped lang='scss'>
+  // 分割线
+  .line{
+    padding: 0 10px;
+    margin: 10px 0;
+    height: 1px;
+    background:#f4f4f4;
+  }
+</style>
