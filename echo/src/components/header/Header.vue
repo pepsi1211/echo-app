663376cfa3f903550a2a3dee86a2272a65ab63c1
pepsi1211 <715803334@qq.com>
2019-10-23 08:30:01 +0800

    2019/10/23 8:29

--- echo/src/components/header/Header.vue
+++ echo/src/components/header/Header.vue
@@ -1,14 +1,6 @@
 <template>
   <main>
-    <van-nav-bar>
-      <van-icon name="../../../public/img/header/back_gray.png" slot="left">
-      </van-icon>
-      <van-icon name="../../../public/img/header/ic_main_title_icon.png" slot="title">
-      </van-icon>
-      <van-icon name="../../../public/img/header/disk.png" slot="right">
-      <!-- <van-icon name="../../../public/img/header/ic_search_black.png" slot="right"> -->
-      </van-icon>
-    </van-nav-bar>
+
   </main>
 </template>
 <script>
@@ -25,7 +17,5 @@ computed: {
 }
 </script>
 <style lang="scss">
-.pic-back{
-  width:.5rem;
-}
+
 </style>
